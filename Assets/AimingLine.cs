bf3593e0d2a83edfceb597e684e24ad87a2d5148
Rick Batka <rick.batka@gmail.com>
2015-07-25 14:00:00 -0700

    aiming lines

---
+++ Assets/AimingLine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Aiming
+{
+	class AimingLine : MonoBehaviour
+	{
+		public float TimePerSegmentInSeconds = 0.1f;
+		//public float MaxTravelDistance = 10f;
+		public int MaxPoints = 100;
+		public float AnimateSpeed = 2.0f;
+
+		private LineRenderer _lineRenderer;
+		private Vector3[] lastPoints;
+
+		void Start()
+		{
+			_lineRenderer = GetComponent<LineRenderer>();
+			_lineRenderer.useWorldSpace = true;
+		}
+
+		void Update()
+		{
+
+		}
+		public void kill() 
+		{
+			_lineRenderer.SetVertexCount(0);
+		}
+		public void UpdateTrajectory(Vector3 initialPosition, Vector3 initialVelocity)
+		{
+			var points = new List<Vector3>();
+			Vector3 currentPosition = initialPosition;
+			Vector3 velocity = initialVelocity;
+			//float distanceTraveled = 0f;
+			int numPoints = 0;
+			while (numPoints < MaxPoints) // && distanceTraveled < MaxTravelDistance && 
+			{
+				var newPosition = currentPosition + velocity * TimePerSegmentInSeconds + 0.5f * Physics.gravity * TimePerSegmentInSeconds * TimePerSegmentInSeconds;
+				//distanceTraveled += (newPosition - currentPosition).magnitude;
+				points.Add(newPosition);
+				currentPosition = newPosition;
+				velocity += Physics.gravity * TimePerSegmentInSeconds;
+				numPoints++;
+			}
+			var newPoints = points.ToArray();
+			animateLineTo(newPoints);
+			lastPoints = newPoints;
+		}
+
+		private void animateLineTo(Vector3[] newPoints)
+		{
+			if (lastPoints != null)
+			{
+				var numSharedPoints = Math.Min(lastPoints.Length, newPoints.Length);
+				for (int i = 0; i < numSharedPoints; i++)
+				{
+					newPoints[i] = iTween.Vector3Update(lastPoints[i], newPoints[i], AnimateSpeed);
+				}
+			}
+
+			_lineRenderer.SetVertexCount(newPoints.Length);
+			for (int i = 0; i < newPoints.Length; i++)
+			{
+				_lineRenderer.SetPosition(i, newPoints[i]);
+			}
+		}
+	}
+}
---
+++ Assets/Pauser.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pauser : MonoBehaviour {
+	GameObject CardCanvas;
+	bool IsPaused = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		CardCanvas = GameObject.Find("CardCanvas");
+		CardCanvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			IsPaused = !IsPaused;
+			Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+			CardCanvas.SetActive(!CardCanvas.activeInHierarchy);
+		}
+	}
+}
